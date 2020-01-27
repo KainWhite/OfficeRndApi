13bcbe78057e8674dde954fd3fc7c82121bc8da6
KainWhite <kain.svetly@gmail.com>
2020-01-28 00:14:22 +0300

    Implement GetToken

--- OfficeRNDApi/OfficeRndApiHandler.cs
+++ OfficeRNDApi/OfficeRndApiHandler.cs
@@ -1,9 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 
 namespace OfficeRndApi {
-  class OfficeRndApiHandler {
-    
+  public static class OfficeRndApiHandler {
+    public class Token {
+      public string access_token { get; set; }
+      public string token_type { get; set; }
+      public int expires_in { get; set; }
+      public string scope { get; set; }
+    }
+    public static Token GetToken(string clientId, string clientSecret) {
+      using (WebClient wc = new WebClient()) {
+        wc.Headers[HttpRequestHeader.ContentType] =
+            "application/x-www-form-urlencoded";
+        try {
+          string res = wc.UploadString(
+              "https://identity.officernd.com/oauth/token",
+              "POST",
+              $"client_id={clientId}&" +
+              $"client_secret={clientSecret}&" +
+              "grant_type=client_credentials&" +
+              "scope=officernd.api.read officernd.api.write"
+          );
+          return JsonSerializer.Deserialize<Token>(res);
+        } catch (WebException e) {
+          Console.WriteLine(e.Message);
+          return null;
+        }
+      }
+    }
+    public static void GetData() {
+      throw new NotImplementedException();
+    }
   }
 }
--- OfficeRNDApi/Program.cs
+++ OfficeRNDApi/Program.cs
@@ -4,6 +4,9 @@ namespace OfficeRndApi {
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
+      var token = OfficeRndApiHandler.GetToken(
+          "o2cBTqaINCOWVBCd", "89c7n4zyKuegDE0wA2J40oDgcXmVUQvH");
+      Console.WriteLine(token.access_token);
     }
   }
 }
